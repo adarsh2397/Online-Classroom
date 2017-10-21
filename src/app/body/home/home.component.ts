69aac61931193e1d48f46cc2fbc3cce47dec2604
Adarsh Honawad <adarsh2397@gmail.com>
2017-10-21 10:14:10 +0530

    Home Design Made Better

--- src/app/body/home/home.component.ts
+++ src/app/body/home/home.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private screenHeight: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.screenHeight = screen.height*0.80 + 'px';
+    console.log(this.screenHeight);
   }
 
 }
